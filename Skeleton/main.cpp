6a0b6c7b06b06da4b4e7fa480ed4a7b347d245b5
andydayo <2016198@s.asojuku.ac.jp>
2022-04-21 14:11:17 +0900

    スケルトン

--- Skeleton/main.cpp
+++ Skeleton/main.cpp
@@ -1,14 +1,22 @@
 #include <DxLib.h>
+#include <time.h>
+
+constexpr int SCREEN_SIZE_X = 640;
+constexpr int SCREEN_SIZE_Y = 480;
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
+	SetOutApplicationLogValidFlag(false);
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
 	SetWindowText("2016198_“›ˆäˆÇ“ñ");
 	ChangeWindowMode(true);
-	if (DxLib_Init())
+	if (DxLib_Init() == -1)
 	{
 		return -1;
 	}
-	SetGraphMode(640, 480, 32);
+
+	//—”‚Ì‰Šú‰»
+	srand((unsigned int)time(NULL));
 
 	//•Ï”‚Ìé“`
 
